723571fa7d40324a849fb7bb40a5bbaff235aa2b
ChuquEmeka <edeh.emeka84@gmail.com>
2026-02-03 21:08:01 +0100

    chore: repository hygiene cleanup

--- target/compiled/dbt_transformations/emekamarkt/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from "dev"."public"."my_first_dbt_model"
-where id = 1
--- target/run/dbt_transformations/emekamarkt/models/example/my_second_dbt_model.sql
+++
@@ -1,9 +0,0 @@
-
-
-  create view "dev"."public"."my_second_dbt_model__dbt_tmp" as (
-    -- Use the `ref` function to select from other models
-
-select *
-from "dev"."public"."my_first_dbt_model"
-where id = 1
-  ) ;
